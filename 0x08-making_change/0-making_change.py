1997b18bb97746ecc2ca2b8b3de97933b7f814b7
amutsamoses <amutsamoses@gmail.com>
2024-08-22 14:51:49 -0700

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,49 +1,30 @@
 #!/usr/bin/python3
-""" Making changes """
+"""
+Make change task
+"""
 
 
 def makeChange(coins, total):
+
+    """ Checks how minimum number of coins needed
+        to meet the total amount.
+        Args:
+            coins: list of coins denominations to use
+            total: amount of change required
+        Return:
+            Minimum number of coins needed to make change.
     """
-    Determines the fewest number of coins needed to meet a given total amount.
-
-    Args:
-        coins (list): List of integers of the coins available
-        total (int): total number to be met
-
-    Returns:
-        int: fewest number of coins needed to meet the total.
-        0: if the total is 0 or less.
-        -1: if the total cannot be met with the available coins.
-
-    """
-
-    '#0 or less, no coins needed'
     if total <= 0:
         return 0
-
-    '#initialize current sum(check) and coin count(temp)'
-    check = 0
-    temp = 0
-
-    '#sort the in desc'
     coins.sort(reverse=True)
+    coins_needed = 0
+    for coin in coins:
+        if total / coin > 0:
+            coins_needed = coins_needed + (total // coin)
+            total = total % coin
+        if not total:
+            break
 
-    '#iterate through each sorted coins'
-    for i in coins:
-
-        while check < total:
-
-            check += i
-
-            temp += 1
-
-    '#return coount of coins used if equal'
-    if check == total:
-        return temp
-
-        '#if check exceed the total, subtract and decrease coin'
-        check -= i
-        temp -= 1
-
-    '#if no exact coin combination is meet'
-    return -1
+    if total != 0 or coins_needed == 0:
+        return -1
+    return coins_needed
